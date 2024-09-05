d80ba914433b61825f040568994d3b207bd33f47
uhammadziyo <DilafruzIminova71@gmail.com>
2024-09-05 10:06:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// let car = { name: `bmw i8`, year: 2014, probeg: `21100`, color: `blue and whihte`, boyi: `1298mm`, eni: `1942mm`, tezligi: `250km/h`, shina: `2800mm` };
+// let user = { name: `Алехандро `, boy: `180 см`, yoshi: 20, klubi: `Манчестер Юнайтед`, tugilganjoyi: `Argentina,ispaniya`, dastijeniyalarii: `kубка Америки: 2024`, lastname: `Гарначо Феррейра`, sotiladi: `45million euro` }
+let player = {}
+player.ism = prompt(`ism kiriting`)
+player.age = prompt(`space idni kiriting`)
+player.davlati = prompt(`parolingizni kiriting`)
+console.log(player);
+
